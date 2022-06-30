9969c7e7df8025012eaec440f2b8f32a6c145466
ManuelTano <manueltano98@gmail.com>
2022-06-30 16:33:34 +0200

    MILESTONE 3

--- js/script.js
+++ js/script.js
@@ -4,13 +4,19 @@
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro
 // ed emetto un messaggio in console con il numero della cella cliccata.
 
+// Prendo l'elemento dal DOM
+
 const grid = document.getElementById('griglia');
 
-function createCell () {
+
+function createCell (content) {
     const cell = document.createElement ('div');
     cell.className = 'cell';
+    cell.innerText = content;
     return cell;
 }
+
+// Impostazioni iniziali
 
 const rows = 10;
 const cells = 10;
@@ -18,9 +24,12 @@ const totalCell = rows * cells;
 
 const button = document.getElementById('play');
 
+// Tramite un click, genero la griglia
+
 button.addEventListener('click', function() {
     for (let i = 1; i <= totalCell; i++ ) {
-        const cella =  createCell();
+        const cella =  createCell(i);
         grid.appendChild(cella);
     }
+
 })
