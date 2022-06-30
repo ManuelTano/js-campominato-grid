0f9e9a9e265437690c6c272cc5209b91c8b37490
ManuelTano <manueltano98@gmail.com>
2022-06-30 15:52:42 +0200

    MILESTONE 2

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 // L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-//Ogni cella ha un numero progressivo, da 1 a 100.
+// Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro
 // ed emetto un messaggio in console con il numero della cella cliccata.
+
+const grid = document.getElementById('griglia');
+
+function createCell () {
+    const cell = document.createElement ('div');
+    cell.className = 'cell';
+    return cell;
+}
+
+const rows = 10;
+const cells = 10;
+const totalCell = rows * cells;
+
+const button = document.getElementById('play');
+
+button.addEventListener('click', function() {
+    for (let i = 1; i <= totalCell; i++ ) {
+        const cella =  createCell();
+        grid.appendChild(cella);
+    }
+})
