24474af2e97ef66997f8da81617f4a98f73db771
ManuelTano <manueltano98@gmail.com>
2022-07-02 13:00:43 +0200

    bonus completed

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -7,68 +7,75 @@
 
 // Impostazioni iniziali
 
-
-const grid = document.getElementById('griglia');
-
-grid.innerHTML = "";
-
-const level = document.getElementById('level').value;
-let rows;
-let cells;
-
-switch (level) {
-    case 'easy': 
-    default:
-    rows = 10;
-    cells = 10;
-    break;
-    case 'normal': 
-    rows = 9;
-    cells = 9;
-    break;
-    case 'hard': 
-    rows = 7;
-    cells = 7;
-    break;
-}
-
-const totalCells = rows * cells;
-
-
-function createCell(cellNumber, cellsPerRow) {
-    const cell = document.createElement('div');
-    cell.className = 'cell';
-    cell.innerText = cellNumber;
-
-    const sideLength = `calc(100% / ${cellsPerRow})`
-    cell.style.height = sideLenght;
-    cell.style.width = sideLenght;
-
-    return cell;
-}
-
-
 const button = document.getElementById('play');
+console.log(level);
 
 // Tramite un click, genero la griglia
 
 button.addEventListener('click', function () {
+
+    let rows;
+    let cells;
+
+    const level = document.getElementById('level').value;
+    
+    switch (level) {
+        case 'easy': 
+        default:
+        rows = 10;
+        cells = 10;
+        break;
+        case 'normal': 
+        rows = 9;
+        cells = 9;
+        break;
+        case 'hard': 
+        rows = 7;
+        cells = 7;
+        break;
+    }
+    
+    const totalCell= rows * cells;
+
+    const grid = document.getElementById('griglia');
+
+    grid.innerHTML = "";
+
+    function createCell(cellNumber, cellsPerRow) {
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+        cell.innerText = cellNumber;
+    
+        const sideLength = `calc(100% / ${cellsPerRow})`
+        cell.style.height = sideLength;
+        cell.style.width = sideLength;
+    
+        return cell;
+    }
     
 
+    function onCellClick() {
+        if (this.classList.contains('clicked')) return;
+        
+        this.classList.add('clicked');
+        console.log(this.innerText);
+    }
+    
+
+    // SVOLGIMENTO DEL PROGRAMMA
+    
     for (let i = 1; i <= totalCell; i++) {
 
-        const cell = createCell(i, cells);
-
-        grid.appendChild(cell);
-
-        cell.addEventListener('click', function () {
-
-        this.classList.toggle('clicked');
+        const cella = createCell(i, cells);
         
-        console.log('Hai cliccato il numero: ' + cell.innerHTML)
-        })
-    }
-    button.innerHTML = 'Ricomincia';
+        cella.addEventListener('click', onCellClick)
 
+        grid.appendChild(cella);
+        
+        }
+
+    
+    button.innerHTML = 'Ricomincia';
+    
 })
 
